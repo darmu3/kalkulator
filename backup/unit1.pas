f68d4e603ff05f36d37b14dcc273ca3bc1e57b70
Александр Сунцов <117808824+darmu3@users.noreply.github.com>
2023-03-17 09:00:13 +0300

    Add files via upload

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -31,7 +31,6 @@ type
     Button29: TButton;
     Button31: TButton;
     Edit1: TEdit;
-    Label1: TLabel;
     procedure Button11Click(Sender: TObject);
     procedure Button12Click(Sender: TObject);
     procedure Button13Click(Sender: TObject);
@@ -49,8 +48,9 @@ type
     procedure Button27Click(Sender: TObject);
     procedure Button28Click(Sender: TObject);
     procedure Button29Click(Sender: TObject);
-    procedure Button30Click(Sender: TObject);
     procedure Button31Click(Sender: TObject);
+    procedure Edit1Change(Sender: TObject);
+
   private
 
   public
@@ -72,33 +72,29 @@ implementation
 procedure TForm1.Button20Click(Sender: TObject);
 begin
   Edit1.Text:=Edit1.Text + '1';
-  Label1.Caption:= Label1.Caption + '1';
 end;
 
 procedure TForm1.Button19Click(Sender: TObject);
 var j, i:string;
 begin
   j:=Edit1.Text;
-  i:=Label1.Caption;
   if j <> '' then Delete(j, Length(j), 1);
-  if i <> '' then Delete(i, Length(i), 1);
   Edit1.Text:=j;
-  Label1.Caption:=i;
 end;
 
 procedure TForm1.Button16Click(Sender: TObject);
 begin
   Edit1.Clear;
-  Label1.Caption := '';
 end;
 
 procedure TForm1.Button18Click(Sender: TObject);
 begin
   if Edit1.Text = '' then abort;
-  Label1.Caption := Edit1.Text;
   ch1 := StrToFloat(Edit1.Text);
+  if ch1 > 0 then
   rez := sqrt(ch1);
   Edit1.Text:=FloatToStr(rez);
+  if ch1<0 then Edit1.Text:='';
 end;
 
 procedure TForm1.Button11Click(Sender: TObject);
@@ -106,7 +102,6 @@ begin
   if Edit1.Text = '' then abort;
   ch1:=StrToFloat(Edit1.Text);
   znak:= '+';
-  Label1.Caption := Label1.Caption + znak;
   Edit1.Clear;
 end;
 
@@ -115,7 +110,6 @@ begin
   if Edit1.Text = '' then abort;
   ch1:=StrToFloat(Edit1.Text);
   znak:= '-';
-  Label1.Caption := Label1.Caption + znak;
   Edit1.Clear;
 end;
 
@@ -124,7 +118,6 @@ begin
   if Edit1.Text = '' then abort;
   ch1:=StrToFloat(Edit1.Text);
   znak:= '*';
-  Label1.Caption := Label1.Caption + znak;
   Edit1.Clear;
 end;
 
@@ -133,75 +126,58 @@ begin
   if Edit1.Text = '' then abort;
   ch1:=StrToFloat(Edit1.Text);
   znak:= '/';
-  Label1.Caption := Label1.Caption + znak;
   Edit1.Clear;
 end;
 
 procedure TForm1.Button21Click(Sender: TObject);
 begin
   Edit1.Text:=Edit1.Text + '2';
-  Label1.Caption:= Label1.Caption + '2';
 end;
 
 procedure TForm1.Button22Click(Sender: TObject);
 begin
   Edit1.Text:=Edit1.Text + '3';
-  Label1.Caption:= Label1.Caption + '3';
 end;
 
 procedure TForm1.Button23Click(Sender: TObject);
 begin
   Edit1.Text:=Edit1.Text + '4';
-  Label1.Caption:= Label1.Caption + '4';
 end;
 
 procedure TForm1.Button24Click(Sender: TObject);
 begin
   Edit1.Text:=Edit1.Text + '5';
-  Label1.Caption:= Label1.Caption + '5';
 end;
 
 procedure TForm1.Button25Click(Sender: TObject);
 begin
   Edit1.Text:=Edit1.Text + '6';
-  Label1.Caption:= Label1.Caption + '6';
 end;
 
 procedure TForm1.Button26Click(Sender: TObject);
 begin
   Edit1.Text:=Edit1.Text + '7';
-  Label1.Caption:= Label1.Caption + '7';
 end;
 
 procedure TForm1.Button27Click(Sender: TObject);
 begin
   Edit1.Text:=Edit1.Text + '8';
-  Label1.Caption:= Label1.Caption + '8';
 end;
 
 procedure TForm1.Button28Click(Sender: TObject);
 begin
   Edit1.Text:=Edit1.Text + '9';
-  Label1.Caption:= Label1.Caption + '9';
 end;
 
 procedure TForm1.Button29Click(Sender: TObject);
 begin
   Edit1.Text:=Edit1.Text + '0';
-  Label1.Caption:= Label1.Caption + '0';
-end;
-
-procedure TForm1.Button30Click(Sender: TObject);
-begin
-  Edit1.Text:=Edit1.Text + ',';
-  Label1.Caption := Label1.Caption + ',';
 end;
 
 procedure TForm1.Button31Click(Sender: TObject);
 begin
   if Edit1.Text = '' then abort;
   ch2 := StrToFloat(Edit1.Text);
-  Label1.Caption := Label1.Caption + '=';
   Edit1.Clear;
   if znak = '+' then
   begin
@@ -217,12 +193,19 @@ begin
   end;
   if znak = '/' then
   begin
-    if ch2 = 0 then begin Edit1.Text:='/ на 0 нельзя!'; Abort; end;
+    if ch2 = 0 then begin ShowMessage('Деление на ноль невозможно!'); Abort; end;
     rez := ch1 / ch2;
   end;
   Edit1.Text:= FloatToStr(rez);
-  Label1.Caption := Label1.Caption + Edit1.Text;
 end;
+
+procedure TForm1.Edit1Change(Sender: TObject);
+begin
+  Edit1.MaxLength:=5;
+end;
+
+
+
 
 
 
